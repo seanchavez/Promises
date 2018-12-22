c80c3d205a7a86aa2c413e0cc2d8ef86eb6f6cc5
Sean Chavez <realseanchavez@gmail.com>
2018-12-21 21:50:15 -0800

    chain

--- src/index.js
+++ src/index.js
@@ -3,6 +3,17 @@ const API_URL = 'https://starwars.egghead.training/';
 
 const responsePromise = fetch(API_URL + 'films'); //axios.get(API_URL + 'films');
 console.log(responsePromise);
-responsePromise.then(response => {
-  console.log(response);
-});
+responsePromise
+  .then(response => {
+    console.log(response);
+    return response.json();
+  })
+  .then(films => {
+    console.log(films);
+    const filmTitles = films
+      .map(film => `${film.episode_id}. ${film.title}`)
+      .join('\n');
+
+    const output = document.getElementById('output');
+    output.innerText = filmTitles;
+  });
