7e317d153d7dec8f2d00bdd4120d6a81d534bdd8
Sean Chavez <realseanchavez@gmail.com>
2018-12-21 20:50:47 -0800

    response

--- src/index.js
+++ src/index.js
@@ -1,5 +1,8 @@
-const axios = require('axios');
+//const axios = require('axios');
 const API_URL = 'https://starwars.egghead.training/';
 
-const responsePromise = axios.get(API_URL + 'films');
+const responsePromise = fetch(API_URL + 'films'); //axios.get(API_URL + 'films');
 console.log(responsePromise);
+responsePromise.then(response => {
+  console.log(response);
+});
