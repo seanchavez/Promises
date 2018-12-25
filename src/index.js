d71cf4a27017f49062533911a3d58584e89dec3e
Sean Chavez <realseanchavez@gmail.com>
2018-12-24 23:33:38 -0800

    cheaze

--- src/index.js
+++ src/index.js
@@ -158,17 +158,30 @@ function queryAPI(endpoint) {
 //     spinner.remove();
 //   });
 
-Promise.all([queryAPI('films'), queryAPI('planets'), queryAPI('species')])
-  .then(([films, planets, species]) => {
-    output.innerText =
-      `${films.length} films, ` +
-      `${planets.length} planets, ` +
-      `${species.length} species`;
-  })
-  .catch(error => {
-    console.warn(error);
-    output.innerText = ':(';
-  })
-  .finally(() => {
-    spinner.remove();
+// Promise.all([queryAPI('films'), queryAPI('planets'), queryAPI('species')])
+//   .then(([films, planets, species]) => {
+//     output.innerText =
+//       `${films.length} films, ` +
+//       `${planets.length} planets, ` +
+//       `${species.length} species`;
+//   })
+//   .catch(error => {
+//     console.warn(error);
+//     output.innerText = ':(';
+//   })
+//   .finally(() => {
+//     spinner.remove();
+//   });
+
+const API_URL = 'https://starwars.egghead.training';
+
+const output = document.getElementById('output');
+const spinner = document.getElementById('spinner');
+
+function queryAPI(endpoint) {
+  return fetch(API_URL + endpoint).then(response => {
+    return response.ok
+      ? response.json()
+      : Promise.reject(Error('Unsuccessful response'));
   });
+}
